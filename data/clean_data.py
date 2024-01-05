f97ae02b016d09e863c740ef6d629d56e91419bb
tmikk11 <57267551+tmikk11@users.noreply.github.com>
2024-01-04 20:06:02 -0800

    Add files via upload

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,29 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+# this notebook cleans data, adds helpful columns, saves as .csv for past 30 years of data
+
+
+# In[42]:
+
+
 # packages
 import pandas as pd
 
-# seatac_noaa.csv is data from https://www.ncdc.noaa.gov/cdo-web/
-# this notebook cleans data, saves as seatac_cleaned.csv for past 30 years of data
 
-# Loading data
-data = pd.read_csv('seatac_noaa.csv')
+# In[43]:
+
+
+# data is last 30 years of daily highs, mins, and precipitation 
+# for Boston (Logan) and Seattle (Sand Point) 
+# from https://www.ncdc.noaa.gov/cdo-web
+
+# Loading data (12/31/2023 wan't readt for seattle yet)
+seattle = pd.read_csv('seattle_sand_point.csv')
+seattle.loc[len(seattle.index)] = ['USW00094290', '2023-12-31', 0.06, 40.0, 51.0]
+boston = pd.read_csv('boston_logan.csv')
 
 # Cleaning headers
-data = data[['DATE', 'PRCP', 'TMIN', 'TMAX']]
-data.rename(columns={"DATE": "date", "PRCP": "prcp", "TMAX": "tmax", "TMIN": "tmin"}, inplace=True)
+seattle = seattle[['DATE', 'PRCP', 'TMIN', 'TMAX']]
+seattle.rename(columns={"DATE": "date", "PRCP": "prcp", "TMAX": "tmax", "TMIN": "tmin"}, inplace=True)
+boston = boston[['DATE', 'PRCP', 'TMIN', 'TMAX']]
+boston.rename(columns={"DATE": "date", "PRCP": "prcp", "TMAX": "tmax", "TMIN": "tmin"}, inplace=True)
 
 # Adding date columns
-data['date'] = pd.to_datetime(data['date'])
-data['year'] = data['date'].dt.year
-data['month'] = data['date'].dt.month
-data['day'] = data['date'].dt.day
-data['dayyear'] = data['date'].dt.dayofyear
-data['season'] = [1 if m in [12,1,2] else 2 if m in [3,4,5] else 3 if m in [6,7,8] else 4 for m in data['month']]
+seattle['date'] = pd.to_datetime(seattle['date'])
+seattle['year'] = seattle['date'].dt.year
+seattle['month'] = seattle['date'].dt.month
+seattle['day'] = seattle['date'].dt.day
+seattle['dayyear'] = seattle['date'].dt.dayofyear
+seattle['season'] = [1 if m in [12,1,2] else 2 if m in [3,4,5] else 3 if m in [6,7,8] else 4 for m in seattle['month']]
+boston['date'] = pd.to_datetime(boston['date'])
+boston['year'] = boston['date'].dt.year
+boston['month'] = boston['date'].dt.month
+boston['day'] = boston['date'].dt.day
+boston['dayyear'] = boston['date'].dt.dayofyear
+boston['season'] = [1 if m in [12,1,2] else 2 if m in [3,4,5] else 3 if m in [6,7,8] else 4 for m in boston['month']]
 
-# Filling missing prcp values
-data.at[10944,'prcp'] = 0.13
-data.at[10956,'prcp'] = 0.11
-data['prcp'].fillna(value=0, inplace=True)
+# missing prcp data is all for days with no rainfall
+boston['prcp'].fillna(value=0, inplace=True)
+seattle['prcp'].fillna(value=0, inplace=True)
+
+# missing temp values for seattle from https://www.wunderground.com/dashboard/pws/KWASEATT232/
+seattle.at[10014,'tmin'] = 61.0
+seattle.at[10015,'tmin'] = 57.0
+seattle.at[10486,'tmin'] = 36.0
+seattle.at[10487,'tmin'] = 37.0
+seattle.at[10487,'tmax'] = 53.0
+seattle.at[10488,'tmin'] = 43.0
+seattle.at[10488,'tmax'] = 50.0
+seattle.at[10489,'tmin'] = 38.0
+seattle.at[10489,'tmax'] = 50.0
+seattle.at[10490,'tmin'] = 40.0
+seattle.at[10490,'tmax'] = 57.0
 
 # Saving last 30 years to dataframe
-data = data[data['year'] > 1992]
-data.to_csv('seatac_cleaned.csv', index=False)
+boston.to_csv('boston_logan_cleaned.csv', index=False)
+seattle.to_csv('seattle_sand_point_cleaned.csv', index=False)
+
+
+# In[ ]:
+
+
+
+
